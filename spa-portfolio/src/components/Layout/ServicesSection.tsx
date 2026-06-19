d7b7bcc468f5d6c7e5c5233b303909df74494db3
maha404 <maria_halvarsson@hotmail.se>
2026-06-19 17:14:30 +0200

    Refactor service card classes for consistency and improved styling

--- spa-portfolio/src/components/Layout/ServicesSection.tsx
+++ spa-portfolio/src/components/Layout/ServicesSection.tsx
@@ -9,12 +9,12 @@ import MotionGraphicsImage from "../../assets/pattern-motion-graphics.svg";
 export default function ServicesSection() {
   return (
     <section className="services-section">
-        <ServicesCard title="Graphic Design" image={GraphicDesignimage} className="graphic-design-card" />
-        <ServicesCard title="UI/UX" image={UIImage} className="ui-card" />
-        <ServicesCard title="Apps" image={AppsImage} className="apps-card" />
-        <ServicesCard title="Illustrations" image={IllustrationsImage} className="illustrations-card" />
-        <ServicesCard title="Photography" image={PhotographyImage} className="photography-card" />
-        <ServicesCard title="Motion Graphics" image={MotionGraphicsImage} className="motion-graphics-card" />
+        <ServicesCard title="Graphic Design" image={GraphicDesignimage} className="service-card graphic-design-card" />
+        <ServicesCard title="UI/UX" image={UIImage} className="service-card ui-card" />
+        <ServicesCard title="Apps" image={AppsImage} className="service-card apps-card" />
+        <ServicesCard title="Illustrations" image={IllustrationsImage} className="service-card illustrations-card" />
+        <ServicesCard title="Photography" image={PhotographyImage} className="service-card photography-card" />
+        <ServicesCard title="Motion Graphics" image={MotionGraphicsImage} className="service-card motion-graphics-card" />
     </section>
   )
 
