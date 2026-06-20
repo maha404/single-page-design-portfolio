3f7a9da88f2fd0b179240bc3d78e8348c6936a6e
maha404 <maria_halvarsson@hotmail.se>
2026-06-20 10:09:20 +0200

    Refactor ServicesCard component to accept imageClassName prop and update service card image styles for better responsiveness

--- spa-portfolio/src/components/Layout/ServicesCard.tsx
+++ spa-portfolio/src/components/Layout/ServicesCard.tsx
@@ -2,12 +2,13 @@ interface ServicesCardProps {
   title: string;
   image: string;
   className: string;
+  imageClassName: string;
 }
 
 export default function ServicesCard(props: ServicesCardProps) {
   return (
     <div className={props.className}>
-      <img src={props.image} alt={props.title} className="portfolio-card-image" />
+      <img src={props.image} alt={props.title} className={props.imageClassName} />
       <h2 className="portfolio-card-title">{props.title}</h2>
     </div>
   )
--- spa-portfolio/src/components/Layout/ServicesSection.tsx
+++ spa-portfolio/src/components/Layout/ServicesSection.tsx
@@ -9,12 +9,12 @@ import MotionGraphicsImage from "../../assets/pattern-motion-graphics.svg";
 export default function ServicesSection() {
   return (
     <section className="services-section">
-        <ServicesCard title="Graphic Design" image={GraphicDesignimage} className="service-card graphic-design-card" />
-        <ServicesCard title="UI/UX" image={UIImage} className="service-card ui-card" />
-        <ServicesCard title="Apps" image={AppsImage} className="service-card apps-card" />
-        <ServicesCard title="Illustrations" image={IllustrationsImage} className="service-card illustrations-card" />
-        <ServicesCard title="Photography" image={PhotographyImage} className="service-card photography-card" />
-        <ServicesCard title="Motion Graphics" image={MotionGraphicsImage} className="service-card motion-graphics-card" />
+        <ServicesCard title="Graphic Design" image={GraphicDesignimage} className="service-card graphic-design-card" imageClassName="service-card-image service-card-image--large" />
+        <ServicesCard title="UI/UX" image={UIImage} className="service-card ui-card" imageClassName="service-card-image service-card-image--large" />
+        <ServicesCard title="Apps" image={AppsImage} className="service-card apps-card" imageClassName="service-card-image service-card-image--small" />
+        <ServicesCard title="Illustrations" image={IllustrationsImage} className="service-card illustrations-card" imageClassName="service-card-image service-card-image--small" />
+        <ServicesCard title="Photography" image={PhotographyImage} className="service-card photography-card" imageClassName="service-card-image service-card-image--small" />
+        <ServicesCard title="Motion Graphics" image={MotionGraphicsImage} className="service-card motion-graphics-card" imageClassName="service-card-image service-card-image--small" />
     </section>
   )
 
