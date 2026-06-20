ec861e4116affafaa70e60df12818a791df777a2
maha404 <maria_halvarsson@hotmail.se>
2026-06-20 10:19:49 +0200

    Update alt text for logo image in Nav component for improved accessibility

--- spa-portfolio/src/components/Layout/Nav.tsx
+++ spa-portfolio/src/components/Layout/Nav.tsx
@@ -4,7 +4,7 @@ import Button from '../Buttons/Button'
 export default function Nav() {
     return (
         <nav className="nav">
-            <img src={Logo} alt="logo" className="nav-logo" />
+            <img src={Logo} alt="Design Portfolio logo" className="nav-logo" />
             <Button text="Free Consultation" className="button-dark button" />
         </nav>
     )
